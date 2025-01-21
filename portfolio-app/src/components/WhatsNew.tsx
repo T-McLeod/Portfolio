f0a09de7db04b78d4748eb86c260b3ab95fa6ac6
T-McLeod <tanner89PLP@gmail.com>
2025-01-21 18:33:58 -0500

    Added real data

--- portfolio-app/src/components/WhatsNew.tsx
+++ portfolio-app/src/components/WhatsNew.tsx
@@ -5,16 +5,16 @@ import './WhatsNew.css';
 const WhatsNew: React.FC = () => {
   const posts = [
     {
-      title: 'New Project Launched',
-      message: 'I have just launched a new project. Check it out!',
-      link: 'https://example.com/new-project',
-      image: 'https://via.placeholder.com/150'
+      title: 'Summer 2025: Internship at Capital One',
+      message: 'I have recently accepted a summer internship at Capital One as a software engineer.',
+      link: 'https://www.capitalonecareers.com/get-ahead-with-early-career-programs-for-students',
+      image: '//tbcdn.talentbrew.com/company/234/gst_v1_0/img/logo-capital-one.svg'
     },
     {
-      title: 'Blog Post Published',
-      message: 'Read my latest blog post on web development trends.',
+      title: 'Elected Vice President of Pi Kappa Alpha',
+      message: 'I was elected as the Vice President of the Alpha Alpha chapter of Pi Kappa Alpha, or PIKE, for the upcoming school year.',
       link: 'https://example.com/blog-post',
-      image: 'https://via.placeholder.com/150'
+      image: '//upload.wikimedia.org/wikipedia/en/thumb/9/90/Pi_Kappa_Alpha_Coat_of_Arms.png/220px-Pi_Kappa_Alpha_Coat_of_Arms.png'
     },
     // Add more posts as needed
   ];
