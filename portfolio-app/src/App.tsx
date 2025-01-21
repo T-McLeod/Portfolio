2c7ab08eb98107e59fae7e3dd0eebf494e96d200
T-McLeod <tanner89PLP@gmail.com>
2025-01-21 16:57:23 -0500

    Added navigation bar

--- portfolio-app/src/App.tsx
+++ portfolio-app/src/App.tsx
@@ -2,11 +2,13 @@ import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Interests from './components/Interests';
+import Navbar from './components/Navbar';
 
 function App() {
 
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/interests" element={<Interests />} />
