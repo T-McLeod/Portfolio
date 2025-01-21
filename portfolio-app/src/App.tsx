ad234b2e67aad362b3ad8c9c0a38ecbfa49e9fd9
T-McLeod <tanner89PLP@gmail.com>
2025-01-21 16:56:37 -0500

    Added interests page

--- portfolio-app/src/App.tsx
+++ portfolio-app/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
+import Interests from './components/Interests';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/interests" element={<Interests />} />
       </Routes>
     </Router>
   )
---
+++ portfolio-app/src/components/Interests.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Interests.css';
+
+const Interests: React.FC = () => {
+  return (
+    <div className="interests">
+      <h2>My Interests</h2>
+      <p>I enjoy coding, hiking, reading, and playing video games.</p>
+    </div>
+  );
+};
+
+export default Interests;
