da853d5102bb800f43fef6298d2e0c93943b0fd8
T-McLeod <tanner89PLP@gmail.com>
2025-01-23 17:14:14 -0500

    Added post and project models

--- database/myapp/models.py
+++ database/myapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    link = models.URLField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    message = models.TextField()
+    link = models.URLField(max_length=200)
+    image = models.URLField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
