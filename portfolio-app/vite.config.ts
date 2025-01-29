8b13540bf0cd9b3fd04b48445d2c52ed4d57a5e5
T-McLeod <tanner89PLP@gmail.com>
2025-01-28 22:59:59 -0500

    Added base url config

--- portfolio-app/vite.config.ts
+++ portfolio-app/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/~tm419/',  // Update this line to match your base URL
   plugins: [react()],
 })
